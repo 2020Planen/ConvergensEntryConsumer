f535c87407ef1ba44b1c379f898267213f40ade3
Mathias <mathias.b.k@hotmail.com>
2020-02-03 11:41:14 +0100

    update

--- src/main/java/org/consumer/EntryConsumer.java
+++ src/main/java/org/consumer/EntryConsumer.java
@@ -5,7 +5,6 @@
  */
 package org.consumer;
 
-
 import io.smallrye.reactive.messaging.annotations.Channel;
 import io.smallrye.reactive.messaging.annotations.Emitter;
 import java.io.IOException;
@@ -16,7 +15,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import org.acme.ConfigJsonObject;
+import org.acme.PlayGround;
+
 import org.apache.camel.ProducerTemplate;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
@@ -24,41 +24,46 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
  *
  * @author Mathias
  */
+@ApplicationScoped
+public class EntryConsumer {
 
+    @Incoming("entry")
 
-    @ApplicationScoped
-    public class EntryConsumer {
-        
-        @Inject
-        @Channel("routing")
-        Emitter<String> outgoing;
-        
-        @Inject
-        ProducerTemplate camelProducer;
-        
-        @Incoming("entry")
-        //@Outgoing("routing")
-        public void consumeEntry(String msg) throws IOException, InterruptedException, ExecutionException, TimeoutException {
-            
-            ConfigJsonObject cj = new ConfigJsonObject("entry");
-            cj.convertJsonToEntity(msg);
-            String newMsg = cj.getJsonObjectString();
-            
-            //Store in database
-            System.out.println("\n------------- Sending entry to database... Time: " + LocalTime.now()+ "-------------\n");
-            CompletableFuture future = camelProducer.asyncSendBody("couchdb:http://cis-x.convergens.dk:5984/mmr?username=admin&password=password", newMsg);
-            future.get(5, TimeUnit.SECONDS);
-            if (future.isDone()){
-                System.out.println("\n------------- Succesfully sent to database -------------\n");
-                outgoing.send(newMsg);
-            } else {
-                System.out.println("ELSE ---------------");
-                //TODO find ud af hvad der skal ske
-            }
+    public void handle(String msg) {
+        PlayGround cj = new PlayGround("entry", msg);
+        try {
+            consumeEntry(msg, cj);
+        } catch (Exception e) {
+            System.out.println("----------- in catch --------------- \n\n");
+            cj.handleError(e.getLocalizedMessage());
 
+        }
+    }
+
+    @Inject
+    @Channel("routing")
+    Emitter<String> outgoing;
+
+    @Inject
+    ProducerTemplate camelProducer;
+
+    public void consumeEntry(String msg, PlayGround cj) throws IOException, InterruptedException, ExecutionException, TimeoutException, Exception {
+
+        
+        String newMsg = cj.getJsonMessage().toString();
+
+        //Store in database
+        System.out.println("\n------------- Sending entry to database... Time: " + LocalTime.now() + "-------------\n");
+        CompletableFuture future = camelProducer.asyncSendBody("couchdb:http://cis-x.convergens.dk:5984/mmr?username=admin&password=password", newMsg);
+        System.out.println(" future resolved "+ future.get(5, TimeUnit.SECONDS));
+        if (future.isDone()) {
+            System.out.println("\n------------- Succesfully sent to database -------------\n");
+            outgoing.send(newMsg);
+        } else {
+            System.out.println("Ikke sendt til databasen ---------------");
+            //TODO find ud af hvad der skal ske
         }
 
     }
 
-
-
+}
