85d20a4efa83fe7befca9bb2d0faa238e5dbc53c
Mathias <mathias.b.k@hotmail.com>
2020-02-06 09:02:33 +0100

    ændret til ny version fra archiva

--- src/main/java/org/consumer/EntryConsumer.java
+++ src/main/java/org/consumer/EntryConsumer.java
@@ -5,6 +5,7 @@
  */
 package org.consumer;
 
+import com.google.gson.Gson;
 import io.smallrye.reactive.messaging.annotations.Channel;
 import io.smallrye.reactive.messaging.annotations.Emitter;
 import java.io.IOException;
@@ -15,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import org.acme.PlayGround;
+import org.acme.Message;
 
 import org.apache.camel.ProducerTemplate;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -30,36 +31,37 @@ public class EntryConsumer {
     @Incoming("entry")
 
     public void handle(String msg) {
-        PlayGround cj = new PlayGround("entry", msg);
         try {
-            consumeEntry(msg, cj);
+            consumeEntry(msg);
         } catch (Exception e) {
+            System.out.println(e);
             System.out.println("----------- in catch --------------- \n\n");
-            cj.handleError(e.getLocalizedMessage());
 
         }
     }
 
     @Inject
-    @Channel("routing")
+    @Channel("exit")
     Emitter<String> outgoing;
 
     @Inject
     ProducerTemplate camelProducer;
 
-    public void consumeEntry(String msg, PlayGround cj) throws IOException, InterruptedException, ExecutionException, TimeoutException, Exception {
-
+    public void consumeEntry(String content) throws IOException, InterruptedException, ExecutionException, TimeoutException, Exception {
+        Gson gson = new Gson();
+        Message msg = gson.fromJson(content, Message.class);
+        msg.startLog("database-consumer");
         
-        String newMsg = cj.getJsonMessage().toString();
 
         //Store in database
         System.out.println("\n------------- Sending entry to database... Time: " + LocalTime.now() + "-------------\n");
-        CompletableFuture future = camelProducer.asyncSendBody("couchdb:http://cis-x.convergens.dk:5984/mmr?username=admin&password=password", newMsg);
+        CompletableFuture future = camelProducer.asyncSendBody("couchdb:http://cis-x.convergens.dk:5984/mmr?username=admin&password=password", content);
         System.out.println(" future resolved "+ future.get(5, TimeUnit.SECONDS));
         if (future.isDone()) {
             System.out.println("\n------------- Succesfully sent to database -------------\n");
-            outgoing.send(newMsg);
-        } else {
+            msg.endLog();
+            outgoing.send(gson.toJson(msg));
+        } else  {
             System.out.println("Ikke sendt til databasen ---------------");
             //TODO find ud af hvad der skal ske
         }
